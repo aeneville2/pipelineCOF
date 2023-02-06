8747e9d8da96072addfb61509c9c8efbad852381
aeneville2 <88807529+aeneville2@users.noreply.github.com>
2023-02-06 16:49:56 -0700

    added in basic ui elements
    added title bar with logo and title; tool bar with basemap gallery, layer list, legend, print, and measurement widgets;

--- map.js
+++ map.js
@@ -1,8 +1,14 @@
 require([
     "esri/Map",
     "esri/views/MapView",
+    "esri/widgets/BasemapGallery",
+    "esri/widgets/Legend",
+    "esri/widgets/LayerList",
+    "esri/widgets/Measurement",
+    "esri/widgets/Print",
+    "esri/widgets/Expand",
     "esri/layers/FeatureLayer"
-], function(Map, MapView, FeatureLayer){
+], function(Map, MapView, BasemapGallery, Legend, LayerList, Measurement, Print, Expand, FeatureLayer){
     const map = new Map({
         basemap: "satellite"
     });
@@ -12,6 +18,75 @@ require([
         map: map,
         center: [-111.65, 40.4],
         zoom: 12
+    });
+
+    const basemapGallery = new BasemapGallery({
+        view: view
+    });
+
+    const basemapExpand = new Expand({
+        view: view,
+        content: basemapGallery,
+        expandIconClass: "esri-icon-basemap",
+        container: "toolBar"
+    });
+
+    const legend = new Legend({
+        view: view
+    });
+
+    const legendExpand = new Expand({
+        view: view,
+        content: legend,
+        expandIconClass: "esri-icon-legend",
+        container: "toolBar"
+    });
+
+    const layerList = new LayerList({
+        view: view
+    });
+
+    const layerExpand = new Expand({
+        view: view,
+        content: layerList,
+        expandIconClass: "esri-icon-layer-list",
+        container: "toolBar"
+    });
+
+    const measurement = new Measurement({
+        view: view,
+        linearUnit: "us-feet"
+    });
+
+    const measureExpand = new Expand({
+        view: view,
+        content: measurement,
+        expandIconClass: "esri-icon-measure",
+        container: "toolBar",
+        expanded: false
+    });
+
+    const print = new Print({
+        view: view
+    });
+
+    const printExpand = new Expand({
+        view: view,
+        content: print,
+        expandIconClass: "esri-icon-printer",
+        container: "toolBar"
+    });
+
+    view.ui.add("titleBar","top-left");
+    view.ui.move("zoom","top-left");
+    view.ui.add("toolBar","top-right");
+
+    document.getElementById("toolBar").addEventListener("click",function(){
+        if(measureExpand.expanded === true){
+            measurement.activeTool = "distance";
+        } else if (measureExpand.expanded === false){
+            measurement.activeTool = null;
+        }
     });
 
     const miPipes = new FeatureLayer({
